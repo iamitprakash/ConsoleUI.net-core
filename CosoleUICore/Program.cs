0f25a5536264efcd73f5596dc0725770e29d2e23
amitprakash <amitbcet2k15@outlook.com>
2020-08-11 04:57:12 +0530

    logger added

--- CosoleUICore/Program.cs
+++ CosoleUICore/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
+//DI
+//Logging
+//Settings
 namespace CosoleUICore
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Build()).Enrich.FromLogContext().WriteTo.Console().CreateLogger();
+
+
+            Log.Logger.Information("Application starting");
+
+            var host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+             {
+             }).UseSerilog().Build();
+
+            static void BuildConfig(IConfigurationBuilder builder)
+            {
+                builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT") ?? "Production"}.json", optional: true)
+                    .AddEnvironmentVariables();
+            }
         }
+    }
+    public class GreetingService
+    {
+
     }
 }
